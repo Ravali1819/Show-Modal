ae3e67d547e4f44c29740e43ddb32be8d8f85e04
Ravali <81100442+Ravali1819@users.noreply.github.com>
2021-06-10 17:46:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+"use  strict";
+
+const modal = document.querySelector(".modal");
+const btnCloseModal = document.querySelector(".close__modal");
+const btnsOpenModal = document.querySelectorAll(".modal__button");
+const overlay = document.querySelector(".overlay");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
